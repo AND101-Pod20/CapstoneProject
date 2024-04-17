892e30672af6166b404c9f4aebf90f4c1dc1257a
aditya-phc <aditya.dev@projecthumancity.com>
2024-04-17 17:10:42 -0500

    Search functionality done

--- app/src/main/java/com/example/capstoneproject/PhotosAdapter.kt
+++ app/src/main/java/com/example/capstoneproject/PhotosAdapter.kt
@@ -1,13 +1,33 @@
 package com.example.capstoneproject
 
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.os.Handler
+import android.os.Looper
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.concurrent.Executors
 
 //class PhotosAdapter(private val mPhotos: List<UnsplashPhoto>): RecyclerView.Adapter<PhotosAdapter.ViewHolder>() {
 class PhotosAdapter(private val mPhotos: List<PhotoData>): RecyclerView.Adapter<PhotosAdapter.ViewHolder>() {
@@ -46,6 +66,75 @@ class PhotosAdapter(private val mPhotos: List<PhotoData>): RecyclerView.Adapter<
         Glide.with(holder.itemView)
             .load(photo.URL)
             .into(holder.imageView)
+
+        var mImage: Bitmap?
+        val downloadUrl = photo.downloadLink
+        val myExecutor = Executors.newSingleThreadExecutor()
+        val myHandler = Handler(Looper.getMainLooper())
+
+        fetchButton.setOnClickListener {
+            myExecutor.execute {
+                mImage = mLoad(downloadUrl)
+                myHandler.post {
+                    imageView.setImageBitmap(mImage)
+                    if(mImage!=null){
+                        mSaveMediaToStorage(mImage)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun mLoad(string: String): Bitmap? {
+        val url: URL = mStringToURL(string)!!
+        val connection: HttpURLConnection?
+        try {
+            connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+            val inputStream: InputStream = connection.inputStream
+            val bufferedInputStream = BufferedInputStream(inputStream)
+            return BitmapFactory.decodeStream(bufferedInputStream)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this@PhotosAdapter, "Error", Toast.LENGTH_LONG).show()
+        }
+        return null
+    }
+
+    // Function to convert string to URL
+    private fun mStringToURL(string: String): URL? {
+        try {
+            return URL(string)
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    // Function to save image on the device.
+    // Refer: https://www.geeksforgeeks.org/circular-crop-an-image-and-save-it-to-the-file-in-android/
+    private fun mSaveMediaToStorage(bitmap: Bitmap?) {
+        val filename = "${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            this.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+        fos?.use {
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(this , "Saved to Gallery" , Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
